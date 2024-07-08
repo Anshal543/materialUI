9d24c20912133fff4dc49158e399d31b3fce2279
Anshal543 <sherhasan543@gmail.com>
2024-07-09 00:56:15 +0500

    autocomplete input

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AutoComplete from './autoCompelete/AutoComplete'
+
+const App = () => {
+  return (
+    <>
+    <AutoComplete />
+    </>
+  )
+}
+
+export default App
